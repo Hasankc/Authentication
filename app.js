1b9cc288de2a008e62dd8b9d33d7dad9c370b775
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-05-20 15:53:36 +0300

    Add bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const app = express()
-const encrypt = require("mongoose-encryption")
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
@@ -20,7 +22,7 @@ const userSchema = new mongoose.Schema ({
 })
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFileds: ['passwprd']})
+
 
 const User = new mongoose.model("User", userSchema)
 
@@ -35,17 +37,23 @@ app.get('/register', (req, res) =>{
 })
 
 app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        passowrd: req.body.passowrd
-    })
-    newUser.save((err)=>{
-        if (err) {
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, (err, hash)=> {
+        
+        const newUser = new User({
+            email: req.body.username,
+            passowrd: hash
+        })
+        newUser.save((err)=>{
+            if (err) {
+                console.log(err)
+            }else{
+                res.render("secrets")
+            }
+        })
+        
+    });
+
+
 })
 app.post("/login", (req, res)=>{
     const username = req.body.username
@@ -55,13 +63,18 @@ app.post("/login", (req, res)=>{
             console.log(err)
         }else{
             if (founUser) {
-                if(founUser.passowrd === passowrd) {
-                    res.render("secrets")
+                bcrypt.compare(password, founUser.passowrd, (err, result) => {
+                    if (result == true) {
+                        res.render("secrets")
+                    }
+                })
+                    
                 }
             }
         }
-    })
-})
+)})
+
+
 app.listen(3000, (req, res) =>{
     console.log("listening on 3000")
 })
