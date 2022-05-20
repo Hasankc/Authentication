6d06b6a771a3ac6d24d08c1981a04a7b467182f2
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-05-20 11:34:02 +0300

    security level 2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const app = express()
+const encrypt = require("mongoose-encryption")
 
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
@@ -12,10 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
-const userSchema = { 
+const userSchema = new mongoose.Schema ({
     email: String,
     passowrd: String
-}
+})
+
+const secret = "Thisisourlittelsecret"
+userSchema.plugin(encrypt, {secret: secret, encryptedFileds: ['passwprd']})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -42,7 +46,21 @@ app.post("/register", (req, res)=>{
         }
     })
 })
-
+app.post("/login", (req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+    User.findOne({email: username}, (err, founUser)=>{
+        if (err) {
+            console.log(err)
+        }else{
+            if (founUser) {
+                if(founUser.passowrd === passowrd) {
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+})
 app.listen(3000, (req, res) =>{
     console.log("listening on 3000")
 })
